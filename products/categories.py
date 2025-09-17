b158ccc0feea0df588b93abc2c08df11a19d1da3
RolandoJV <javierrolando0905@gmail.com>
2025-09-17 01:56:15 -0400

    Add files via upload

---
+++ products/categories.py
@@ -0,0 +1,9 @@
+# products/categories.py
+
+CATEGORIES = {
+    'streaming': '📺 Streaming',
+    'music': '🎵 Música',
+    'apple': '🍎 Apple',
+    'licencias': 'Licencias',
+    'vpn': 'VPN'
+}
---
+++ products/products_data.py
@@ -0,0 +1,27 @@
+# products/products_data.py
+
+PRODUCTS = [
+    # Streaming
+    ("Netflix Premium 4k", "streaming", 1800,"1-Mes; 1-Dispositivo","Disfrute de Series y Peliculas de Estreno"),
+    ("HBO Max Platino", "streaming", 1400,"1-Mes; 1-Dispositivo", "Disfrute de Series y Peliculas de Estreno"),
+    ("Disney", "streaming", 1600,"1-Mes; 1-Dispositivo","Disfrute de Series y Peliculas de Estreno"),
+    ("YouTube Premium", "streaming", 1400,"1-Mes; 1-Dispositivo", "Disfrute de Series y Peliculas de Estreno"),
+    ("Prime Video", "streaming", 1300,"1-Mes; 1-Dispositivo", "Disfrute de Series y Peliculas de Estreno"),
+    ("CrunchyRoll", "streaming", 800,"1-Mes; 1-Dispositivo", "Disfrute de Series y Peliculas de Estreno"),
+
+    # Música
+    ("Spotify Premium 1 mes", "music", 1600, "Acceso ilimitado a música","Listas de reproduccion personalizadas a tu Gusto"),
+    ("Apple Music 3 meses", "music", 800, "Música sin anuncios", "Disfruta de la Musica a todas horas, lo mejor funciona sin vpn"),
+
+    # Apple
+    ("Apple One 1 mes","apple",2500,"Disfruta del Paquete completo de Apple y 400gb Icloud","El mejor servicio al Mejor Precio"),
+
+    #Licencias
+    ("Discord Nitro","licencias",5000,"1-Mes; Clave de Digital","Mejoremos la Comunidad Juntos"),
+    ("Discord Basic","licencias",3000,"1-Mes; Clave de Digital","Mejoremos la Comunidad Juntos"),
+    ("Eset-Nod32","licencias",4000,"1-Año; Clave Digital","Tu Pc ahora tendra la mejor proteccion contra las amenazas"),
+
+    #VPN
+    ("Surfshark-VPN","vpn",600,"1-Mes; 1-Dispositivo", "Disfruta de contenido Limitado por tu operadora")
+
+]
